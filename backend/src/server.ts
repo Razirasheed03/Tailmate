421aac782256b3b49e56f0c743fe4483e28eefdc
Razirasheed03 <razirasheed03@gmail.com>
2025-07-08 17:26:23 +0530

    formatted page

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config()
-import {env} from './config/env'
-import  express  from "express";
+import { env } from './config/env'
+import express from "express";
 import { connectDB } from "./config/mongodb";
 import authRoutes from "./routes/auth/authRoute";
 import cors from "cors"
 
-const app=express();
+const app = express();
 
 app.use(cors({
     origin: "http://localhost:3000", // allow Vite frontend
@@ -17,6 +17,6 @@ app.use(express.json())
 connectDB()
 app.use("/api/auth", authRoutes)
 
-app.listen(env.PORT,()=>{
+app.listen(env.PORT, () => {
     console.log(`Server running on ${env.PORT}`)
 })
