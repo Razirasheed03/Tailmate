bbf1a28ed32bcad4e3ae9dbd752995dec55e499a
Razirasheed03 <razirasheed03@gmail.com>
2025-07-13 22:45:11 +0530

    structured pages

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,8 +9,8 @@ import cors from "cors"
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000", 
-    credentials: true,              
+    origin: "http://localhost:3000",
+    credentials: true,
 }));
 
 app.use(express.json())
