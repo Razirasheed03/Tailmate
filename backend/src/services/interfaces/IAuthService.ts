38050642c190cf4d2fac6ee3a2c5f1149150101b
Razirasheed03 <razirasheed03@gmail.com>
2025-08-09 11:46:24 +0530

    structured page

--- backend/src/services/interfaces/IAuthService.ts
+++ backend/src/services/interfaces/IAuthService.ts
@@ -2,9 +2,9 @@ import { IUser } from "../../models/interfaces/IUser";
 
 export interface IAuthService {
   signup(user: IUser): Promise<{ message: string }>;
-  verifyOtp(email: string, otp: string): Promise<{ accessToken: string;refreshToken: string; user: IUser }>;
-    resendOtp(email: string): Promise<void>;
-    refreshToken(refreshToken: string): Promise<{ accessToken: string }>;
-    login(email: string, password: string): Promise<{ accessToken: string; refreshToken: string; user: IUser }>;
+  verifyOtp(email: string, otp: string): Promise<{ accessToken: string; refreshToken: string; user: IUser }>;
+  resendOtp(email: string): Promise<void>;
+  refreshToken(refreshToken: string): Promise<{ accessToken: string }>;
+  login(email: string, password: string): Promise<{ accessToken: string; refreshToken: string; user: IUser }>;
 
 }
