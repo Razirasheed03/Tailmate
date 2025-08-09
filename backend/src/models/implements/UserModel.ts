45fba0c3e6ae43b1c3743b2dcb8c0a5bc7588ebb
Razirasheed03 <razirasheed03@gmail.com>
2025-08-09 11:44:40 +0530

    structured page

--- backend/src/models/implements/UserModel.ts
+++ backend/src/models/implements/UserModel.ts
@@ -10,7 +10,7 @@ const UserSchema = new Schema<IUserDoc>(
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-     isAdmin: { type: Boolean, default: false },
+    isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
