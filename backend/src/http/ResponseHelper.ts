fc8f70f02f1291c2a98085d8007193f34e2416f9
Razirasheed03 <razirasheed03@gmail.com>
2025-09-21 23:14:53 +0530

    changed to message constant

--- backend/src/http/ResponseHelper.ts
+++ backend/src/http/ResponseHelper.ts
@@ -2,6 +2,7 @@
 import { Response } from "express";
 import { ApiResponse, ApiSuccess, ApiError, ErrorItem } from "./response.types";
 import { HttpStatus } from "../constants/httpStatus";
+import { HttpResponse } from "../constants/messageConstant";
 
 export class ResponseHelper {
   static ok<T>(res: Response<ApiResponse<T>>, data: T, message?: string) {
@@ -48,7 +49,7 @@ export class ResponseHelper {
 
   static badRequest(
     res: Response<ApiResponse<never>>,
-    message = "Validation failed",
+    message = HttpResponse.VALIDATION_FAILED,
     errors?: ErrorItem[]
   ) {
     return this.error(
@@ -62,26 +63,26 @@ export class ResponseHelper {
 
   static unauthorized(
     res: Response<ApiResponse<never>>,
-    message = "Unauthorized"
+    message = HttpResponse.UNAUTHORIZED
   ) {
     return this.error(res, HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", message);
   }
 
-  static forbidden(res: Response<ApiResponse<never>>, message = "Forbidden") {
+  static forbidden(res: Response<ApiResponse<never>>, message = HttpResponse.FORBIDDEN) {
     return this.error(res, HttpStatus.FORBIDDEN, "FORBIDDEN", message);
   }
 
-  static notFound(res: Response<ApiResponse<never>>, message = "Not found") {
+  static notFound(res: Response<ApiResponse<never>>, message = HttpResponse.PAGE_NOT_FOUND) {
     return this.error(res, HttpStatus.NOT_FOUND, "NOT_FOUND", message);
   }
 
-  static conflict(res: Response<ApiResponse<never>>, message = "Conflict") {
+  static conflict(res: Response<ApiResponse<never>>, message = HttpResponse.CONFLICT) {
     return this.error(res, HttpStatus.CONFLICT, "CONFLICT", message);
   }
 
   static internal(
     res: Response<ApiResponse<never>>,
-    message = "Internal server error",
+    message = HttpResponse.SERVER_ERROR,
     details?: unknown
   ) {
     return this.error(
