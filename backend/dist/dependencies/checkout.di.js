cc4832a5f28b0dd498cd42df67f226cd5fbf6878
Razirasheed03 <razirasheed03@gmail.com>
2025-12-12 23:01:39 +0530

    deleted unused files

--- backend/dist/dependencies/checkout.di.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkoutController = void 0;
-// backend/src/dependencies/checkout.di.ts
-const checkout_controller_1 = require("../controllers/Implements/checkout.controller");
-const checkout_service_1 = require("../services/implements/checkout.service");
-const checkoutService = new checkout_service_1.CheckoutService();
-exports.checkoutController = new checkout_controller_1.CheckoutController(checkoutService);
